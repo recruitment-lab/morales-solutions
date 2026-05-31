2de878a378ace769a2633c4a375b8e3e1c9e84de
Andreina Di Brigida <Akiarakirax3@gmail.com>
2026-05-31 13:49:33 -0400

    Fix deploy on github-pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const basePath = isGithubPages ? "/morales-solutions" : undefined;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
